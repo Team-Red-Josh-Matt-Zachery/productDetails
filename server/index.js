55ffc7cdd499895e349e988b62b6eae7e5c8e368
Joshua T. Miltier <jomi3524@colorado.edu>
2020-07-20 11:33:31 -0600

    new relic and loader.io setup

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('newrelic');
 const express = require('express');
 const cors = require('cors');
 const router = require('./routes/route.js');
@@ -9,6 +10,9 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.use('/products', router);
+app.get('/loaderio-b5dbc8ec0886d06fe1feba03b86448c3', (req, res) => {
+  res.status(200).send('loaderio-b5dbc8ec0886d06fe1feba03b86448c3');
+});
 
 // eslint-disable-next-line no-console
 // app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
